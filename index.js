57c00cd1c0ab7821c701f1f81d3b18c36d37b1ea
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2018-02-13 23:57:21 +0300

    WIP: Google OAuth

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const clientID = '823166575512-gt41naa8gtts846o081i6agh41s2lt1j.apps.googleusercontent.com';
+const clientSecret = 'Rb5mpJC8s4Vq0KuJWAplfgRc';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({ message: 'test' });
